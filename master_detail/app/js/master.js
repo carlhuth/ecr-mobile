515848409c1397e1d790d396356be167223f5569
pbojinov <petarbojinov@gmail.com>
2014-02-28 11:03:01 -0800

    move around files, add footer nav

--- master_detail/app/js/master.js
+++ master_detail/app/js/master.js
@@ -1,9 +1,18 @@
-function MasterController($scope, Data){
-    $scope.items = Data.items;  
-    
-    $scope.showDetail = function(index){
+function MasterController($scope, Data) {
+    $scope.items = Data.items;
+
+    $scope.showDetail = function(index) {
         var selectedItem = Data.items[index];
         Data.selectedItem = selectedItem;
-        $scope.ons.navigator.pushPage('../detail.html', { title : selectedItem.title });
+        $scope.ons.navigator.pushPage('../detail.html', {
+            title: selectedItem.title
+        });
     }
+
+    // this doesn't work
+    $scope.selectedValue = 'Weekday';
+
+    $scope.$watch('selectedValue', function(newValue, oldValue) {
+        console.log(newValue);
+    });
 }
